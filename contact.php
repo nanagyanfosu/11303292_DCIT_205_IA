f01c97f886a9952eb790b10b0065efa2a0819b24
Theoford Nana Gyanfosu <nanagyanfosu@gmail.com>
2023-11-23 16:28:47 +0000

    added a courses page and edited

---
+++ contact.php
@@ -0,0 +1,23 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$message = $_POST['message'];
+
+$email_from = 'info@dcs.ug.edu.gh';
+
+$email_body = 'user name: $name.\n'.
+              'user email: $visitor_email.\n'.
+              'user message: $message .\n';
+
+$to = 'dcs@ug.edu.gh';
+
+$headers = "from: $email_from \r\n";
+
+$headers = "reply to: $visitor_email \r\n";
+
+mail($to,$email_body,$headers);
+
+header("location: contact.html");
+
+?>
+
